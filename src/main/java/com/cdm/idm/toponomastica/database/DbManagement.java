438aaae41f150240fefe27a1758b16f4b9e2cd11
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-05-06 15:29:03 +0200

    Aggiunto: definizione delle variabili per la connesione al database e metodi get/set

---
+++ src/main/java/com/cdm/idm/toponomastica/database/DbManagement.java
@@ -0,0 +1,72 @@
+package com.cdm.idm.toponomastica.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagement {
+
+    private String ip, user, password, port, nameDb;
+
+    private String PREFIX_URL = "jdbc:postgresql://";
+
+    /* Costruttore */
+    public DbManagement(String ip, String user, String password, String port, String nameDb) {
+        this.ip = ip;
+        this.user = user;
+        this.password = password;
+        this.nameDb = nameDb;
+        this.ip = ip;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getNameDb() {
+        return nameDb;
+    }
+
+    public String getPREFIX_URL() {
+        return PREFIX_URL;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public void setNameDb(String nameDb) {
+        this.nameDb = nameDb;
+    }
+
+    public void setPREFIX_URL(String pREFIX_URL) {
+        PREFIX_URL = pREFIX_URL;
+    }
+
+}
