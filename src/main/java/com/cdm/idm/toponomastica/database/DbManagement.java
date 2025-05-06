4300fa4aa3699c4d55ad52aec92c5ce9f8c4e5ac
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-05-06 15:47:20 +0200

    Aggiunto: aggiunto il metodo che apre e chiude la connessione al database

--- src/main/java/com/cdm/idm/toponomastica/database/DbManagement.java
+++ src/main/java/com/cdm/idm/toponomastica/database/DbManagement.java
@@ -75,7 +75,63 @@ public class DbManagement {
      * @return la url della connessione al database
      */
     public String getUrl() {
-        return getPREFIX_URL() + getIp() + ":" + getPort() + "/" + getNameDb();
+        return this.PREFIX_URL + this.ip + ":" + this.port + "/" + this.nameDb;
+    }
+
+    /**
+     * Metodo che crea e apre la connessione al database
+     * 
+     * @return l'oggetto della connessione al database
+     */
+    public Connection openConnection() {
+
+        // Variabile oggetto connection
+        Connection connection = null;
+
+        String method = "[opeConnection]::";
+
+        try {
+
+            // 1. Caricare il driver JDBC per PostgreSQL
+            Class.forName("org.postgresql.Driver");
+
+            // 2. Stabilire la connessione al database
+            System.out.println(method + "Tentativo di connessione al database...");
+            connection = DriverManager.getConnection(getUrl(), this.user, this.password);
+            System.out.println(method + "Connessione al database stabilita con successo!");
+
+        } catch (ClassNotFoundException e) {
+            System.err.println(method + "Errore: Driver JDBC per PostgreSQL non trovato!");
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            System.err.println(method + "Errore durante la connessione o l'esecuzione della query SQL:");
+            e.printStackTrace();
+        }
+
+        return connection;
+
+    }
+
+    /**
+     * Metodo che chiude la connessione al database
+     * 
+     * @param connection l'oggetto della connessione aperta del database
+     */
+    public void closeConnection(Connection connection) {
+
+        String method = "[closeConnection]::";
+
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println(method + "Connessione al database chiusa.");
+            }
+        } catch (SQLException e) {
+            System.err.println(method + "Errore durante la chiusura della connessione:");
+            e.printStackTrace();
+        }
+
     }
 
 }
