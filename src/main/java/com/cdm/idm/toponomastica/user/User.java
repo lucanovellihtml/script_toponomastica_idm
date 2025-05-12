e570de1ac8516c3763e48d52fb12502c4f57c1d1
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-05-12 17:12:35 +0200

    Aggiornato: aggiunta l'acquisizione dei parametri che andranno salvati su IDM(idc,streetcode,residenceaddressnew,typeviacode)

--- src/main/java/com/cdm/idm/toponomastica/user/User.java
+++ src/main/java/com/cdm/idm/toponomastica/user/User.java
@@ -2,10 +2,44 @@ package com.cdm.idm.toponomastica.user;
 
 public class User {
 
-    private String username, residenceAddress;
+    private String username, residenceAddress, typeViaCode, residenceAddressNew;
+    private Integer streetCode, idc;
+    private boolean updateFlag = false;
 
     public String getUsername() {
         return username;
+    }
+
+    public String getTypeViaCode() {
+        return typeViaCode;
+    }
+
+    public void setTypeViaCode(String typeViaCode) {
+        this.typeViaCode = typeViaCode;
+    }
+
+    public Integer getStreetCode() {
+        return streetCode;
+    }
+
+    public void setStreetCode(Integer streetCode) {
+        this.streetCode = streetCode;
+    }
+
+    public Integer getIdc() {
+        return idc;
+    }
+
+    public void setIdc(Integer idc) {
+        this.idc = idc;
+    }
+
+    public boolean isUpdateFlag() {
+        return updateFlag;
+    }
+
+    public void setUpdateFlag(boolean updateFlag) {
+        this.updateFlag = updateFlag;
     }
 
     public void setUsername(String username) {
@@ -22,7 +56,17 @@ public class User {
 
     @Override
     public String toString() {
-        return "User [username=" + username + ", residenceAddress=" + residenceAddress + "]";
+        return "User [username=" + username + ", residenceAddress=" + residenceAddress + ", typeViaCode=" + typeViaCode
+                + ", streetCode=" + streetCode + ", idc=" + idc + ", updateFlag=" + updateFlag
+                + ", residenceAddressNew=" + residenceAddressNew + "]";
+    }
+
+    public String getResidenceAddressNew() {
+        return residenceAddressNew;
+    }
+
+    public void setResidenceAddressNew(String residenceAddressNew) {
+        this.residenceAddressNew = residenceAddressNew;
     }
 
 }
